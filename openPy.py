2ca592163d583b2833da750abfaebf31d8e481a2
Rehan-Sattar <rehansattar117@gmail.com>
2019-03-09 15:13:19 +0500

    Addition-Update: Bar Chart Added

--- openPy.py
+++ openPy.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
 # creating an instance of file object
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
@@ -9,6 +11,19 @@ for row in range(2, sheet.max_row + 1):
     corrected_price = sheet.cell(row, 3).value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
+
+values = Reference(
+    sheet,
+    min_row=2,
+    max_row=sheet.max_row,
+    min_col=4,
+    max_col=4
+)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'E2')
+
 wb.save('newSheet.xlsx')
 
 
