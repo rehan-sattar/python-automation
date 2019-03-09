f9c4ff9ab35d7ccfb55fe90c381822a3d8da7a86
Rehan-Sattar <rehansattar117@gmail.com>
2019-03-09 15:19:51 +0500

    Formatting- reusable function defined for xls files

--- openPy.py
+++ openPy.py
@@ -2,28 +2,30 @@ import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
 # creating an instance of file object
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-# 2 ways to create a cell object
-# cell = sheet['a1']
-# cell = sheet.cell(1, 1)
-for row in range(2, sheet.max_row + 1):
-    corrected_price = sheet.cell(row, 3).value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+def process_workbook(filename):
 
-values = Reference(
-    sheet,
-    min_row=2,
-    max_row=sheet.max_row,
-    min_col=4,
-    max_col=4
-)
+    wb = xl.load_workbook(filename)
+    # sheets could be duynamic too
+    sheet = wb['Sheet1']
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'E2')
+    for row in range(2, sheet.max_row + 1):
+        corrected_price = sheet.cell(row, 3).value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
 
-wb.save('newSheet.xlsx')
+    values = Reference(
+        sheet,
+        min_row=2,
+        max_row=sheet.max_row,
+        min_col=4,
+        max_col=4
+    )
+
+    chart = BarChart()
+    chart.add_data(values)
+    # injecting column can be dynamic too
+    sheet.add_chart(chart, 'E2')
+
+    wb.save(filename)
 
 
